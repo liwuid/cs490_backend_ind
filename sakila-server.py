2e14e83331b26e446afd6c3e02d9ac149e821776
liwuid <alexiades.v@gmail.com>
2025-09-12 12:54:58 -0400

    •    As a user I want to view top 5 rented films of all times •    As a user I want to be able to click on any of the top 5 films and view its details

--- sakila-server.py
+++ sakila-server.py
@@ -12,16 +12,34 @@ mysql = MySQL(app)
 @app.route('/')
 def home():
     return "Sakila Flask API is running! Go to /allfilms to see films."
- 
-@app.route('/films')
-def films():
+
+@app.route('/topfivefilms')
+def topfivefilms():
     cursor = mysql.connection.cursor()
-    query = """select film_id, title from film limit 10"""
+    query = """
+        select film.film_id, film.title, category.name, count(rental_id) as rented
+        from rental 
+        join inventory on inventory.inventory_id = rental.inventory_id
+        join film on film.film_id = inventory.film_id
+        join film_category on film_category.film_id = film.film_id
+        join category on category.category_id = film_category.category_id
+        group by film.film_id, film.title, category.name
+        order by rented desc, film.title asc
+        limit 5;
+    """
     cursor.execute(query)
     result = cursor.fetchall()
     cursor.close()
     return jsonify(result)
 
+@app.route('/filmdetails/<film_id>')
+def filmdetails(film_id):
+    cursor = mysql.connection.cursor()
+    query = """ select * from film where film_id = %s; """
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
