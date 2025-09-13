1fff7b88ff55b9bd073af83ce00a733c63b0afeb
liwuid <alexiades.v@gmail.com>
2025-09-13 18:25:43 -0400

    As a user I want to be able to view top 5 actors that are part of films I have in the store •    As a user I want to be able to view the actor’s details and view their top 5 rented films

--- sakila-server.py
+++ sakila-server.py
@@ -32,14 +32,74 @@ def topfivefilms():
     cursor.close()
     return jsonify(result)
 
-@app.route('/filmdetails/<film_id>')
+@app.route('/film/<film_id>')
 def filmdetails(film_id):
     cursor = mysql.connection.cursor()
-    query = """ select * from film where film_id = %s; """
+    query = """
+        select film.film_id, film.title, film.description, film.release_year, category.name as category, language.name as language
+        from film
+        join film_category on film.film_id = film_category.film_id
+        join category on category.category_id = film_category.category_id
+        join language on language.language_id = film.language_id
+        where film.film_id = %s;
+    """
     cursor.execute(query)
     result = cursor.fetchall()
     cursor.close()
     return jsonify(result)
+
+@app.route('/topfiveactors')
+def topfiveactors():
+    cursor = mysql.connection.cursor()
+    query = """
+        select actor.actor_id, actor.first_name, actor.last_name, count(film.film_id) as movies
+        from film_actor
+        join actor on actor.actor_id = film_actor.actor_id
+        join film on film.film_id = film_actor.film_id
+        group by actor.actor_id
+        order by movies desc
+        limit 5;
+    """
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify(result)
+
+@app.route('/actor/<actor_id>')
+def actordetails(actor_id):
+    cursor = mysql.connection.cursor()
+    query = """
+        select actor.actor_id, actor.first_name, actor.last_name, count(film.film_id) as movies
+        from film_actor
+        join actor on actor.actor_id = film_actor.actor_id
+        join film on film.film_id = film_actor.film_id
+        where actor.actor_id = %s
+        group by actor.actor_id;
+    """
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify(result)
+
+@app.route('/topfive/actor/<actor_id>')
+def topfiveactorfilms(actor_id):
+    cursor = mysql.connection.cursor()
+    query = """
+        select film.film_id, film.title, count(rental_id) as rental_count
+        from rental
+        join inventory on inventory.inventory_id = rental.inventory_id
+        join film on film.film_id = inventory.film_id
+        join film_actor on film_actor.film_id = film.film_id
+        where film_actor.actor_id = %s
+        group by film.film_id, film.title
+        order by rental_count desc
+        limit 5;
+    """
+    cursor.execute(query, (actor_id,))
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
